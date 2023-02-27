c6624343821e26bdd7a99dc197bc7ecf336cfb8a
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-27 02:42:15 +0000

    product controller

---
+++ product-service/src/main/java/com/rozz/productservice/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.rozz.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rozz.productservice.dto.ProductRequest;
+import com.rozz.productservice.dto.ProductResponse;
+import com.rozz.productservice.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+}
