2ce3d02475b7815d72d44886c1d135cbb03f3bf9
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-27 02:43:09 +0000

    product response dto

---
+++ product-service/src/main/java/com/rozz/productservice/dto/ProductResponse.java
@@ -0,0 +1,20 @@
+// Data transfer Object
+package com.rozz.productservice.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
